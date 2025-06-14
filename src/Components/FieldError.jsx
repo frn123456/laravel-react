df6f83ec421af665c7fa537078a2983bb9b98e75
francis <francisanino126@gmail.com>
2025-06-14 16:38:12 +0800

    changed useLaravelErrors.js

--- src/Components/FieldError.jsx
+++ src/Components/FieldError.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
-const FieldError = ({errors, field}) => {
-    if(!errors?.[field]) return null;
-  return (
-    <p className='text-red-500 text-xs mt-1'>
-        {errors[field][0]}
-    </p>
-  )
-}
+export default function FieldError({ errors, field }) {
+  if (!errors?.[field]) return null;
 
-export default FieldError
+  return (
+    <p className="text-red-500 text-sm mt-1">
+      {errors[field][0]}
+    </p>
+  );
+}
