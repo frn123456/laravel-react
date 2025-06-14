99b083ce47ed259811dc6fd6a9290f08dba6b3ad
francis <francisanino126@gmail.com>
2025-06-15 02:50:06 +0800

    same shit

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -63,9 +63,7 @@ export default function Login() {
       navigate("/");
       console.log(data);
     } catch (error) {
-      if (error.response?.data?.errors) {
-        setErrors(error.response.data.errors);
-      }else if (error.response?.data?.message) {
+      if (error.response?.data?.message) {
         setErrors({ email: [error.response.data.message] });
       }
       console.error("Login error:", error);
